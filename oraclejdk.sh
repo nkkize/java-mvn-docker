1a726f78a371847c4d84db689b57e14176dfff12
Narender Kumar <nkk.snat@gmail.com>
2016-06-13 17:21:46 +0530

    Create oraclejdk.sh

---
+++ oraclejdk.sh
@@ -0,0 +1,5 @@
+export J2SDKDIR=/usr/lib/jvm/oracle_jdk8
+export J2REDIR=/usr/lib/jvm/oracle_jdk8/jre
+export PATH=$PATH:/usr/lib/jvm/oracle_jdk8/bin:/usr/lib/jvm/oracle_jdk8/db/bin:/usr/lib/jvm/oracle_jdk8/jre/bin
+export JAVA_HOME=/usr/lib/jvm/oracle_jdk8
+export DERBY_HOME=/usr/lib/jvm/oracle_jdk8/db
